259dd1463ea61804a741e26a67c4ee12a851fd35
Eduardo de Oliveira <eduardoolive133@gmail.com>
2022-11-07 04:11:49 -0300

    Upgrade na interatividade

--- conditions.js
+++ conditions.js
@@ -2,6 +2,9 @@ var verificar = window.document.getElementById
 ("botao")
 verificar.addEventListener("click", ver)
 //var vereficar vai selecionar o elemento do botão, para que o ouvidor de eventos "escute" o evento click nessa variável, se ele "escutar" esse evento, ele vai executar a função ver(). 
+verificar.addEventListener("mouseenter", entrou)
+verificar.addEventListener("mouseout", saiu)
+
 function ver(){
     var res = window.document.querySelector("div#resultado")
     var dinheiro = Number(txtcl.value)
@@ -12,3 +15,15 @@ function ver(){
         res.innerHTML += "Você clama por <strong>picanha</strong> e <strong>cerveja quentinha</strong>"
     }
 } //Isto é uma condição composta, pelo fato de ter o if e o else. 
+
+function entrou() {
+    verificar.style.color = 'rgb(149, 235, 254)'
+    verificar.style.fontWeight = '700'
+    verificar.style.backgroundColor = 'rgb(60, 74, 74)'
+}
+
+function saiu() {
+    verificar.style.color = 'black'
+    verificar.style.fontWeight = '100'
+    verificar.style.backgroundColor = 'white'
+}
